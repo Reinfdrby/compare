9e4416bdd1867faecd70b18e31095b3f56432300
Reinfdrby <91591522+Reinfdrby@users.noreply.github.com>
2021-11-30 15:15:24 +0300

    Update up_and_down.js

--- up_and_down.js
+++ up_and_down.js
@@ -12,3 +12,14 @@ $(document).ready(function() {
         $('.godown_block').toggleClass('boom').next();
     });
 });
+
+
+
+$(document).ready(function() {
+    $('.left_btn').click(function(my) {
+        document.querySelector('.info1_1').innerHTML =
+        `<p  class='info1_red'>Все пропало <br> на нас движется <b>метеоритный дождь</b>  </p>`
+        var g32 = document.innerHTML = `<p>Живите теперь с этим</p>`;
+        document.querySelector('.info1_1').after(g32);
+    });
+});
